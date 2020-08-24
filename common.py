1f08cac250d18db2abf4a5c65318eb9046543d44
Gabriel <gabriel.andrad4@gmail.com>
2020-08-23 22:28:05 -0300

    August book

--- common.py
+++ common.py
@@ -2,18 +2,16 @@ import os
 token = os.environ.get('TOKEN')
 
 mensagem = {'cronograma' : """
-CRONOGRAMA PESSOAS NORMAIS
-26/07 - 01/08: 24% Capítulo Três Meses Depois (Novembro de 2011)
-02/08 - 08/08: 50% Capítulo Quatro Meses Depois (Janeiro de 2013)
-09/08 - 15/08: 72% Capítulo Três Meses Depois (Março de 2014)
-16/08 - 22/08: 100% Até o final
-Dias de Discussão:
-02/08 às 18h
-09/08 às 18h
-16/08 às 18h
-23/08 às 18h""" ,
+CRONOGRAMA:
+23/08 a 29/08: Ler até o capítulo Segunda Noite (28%)
+30/08 a 05/09: Ler até o capítulo A Vigília (42%)
+06/09 a 12/09: Ler até o capítulo A Primeira Manhã (72%)
+13/09 a  19/09: Ler até o final (100%)
 
-'foto' : 'https://images-na.ssl-images-amazon.com/images/I/71jrp43TCcL.jpg',
+Dias de discussão:
+30/08, 06/09, 13/09 e 20/09 às 18h""" ,
+
+'foto' : 'https://images-na.ssl-images-amazon.com/images/I/91M07A8HuNL.jpg',
 
 'bem-vindo' : """bem-vindo(a) ao Grupo de Leituras dos Entocados. Em caso de spoilers, usar o bot @Hideitbot ou @spoilerobot para esconder a mensagem.
 Para acessar o cronograma e o livro do mês digite os comandos : /livrodomes ou /cronograma"""
